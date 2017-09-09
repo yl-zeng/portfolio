24ae4171b81ee9a9a45bce97bb9dd46c26b70048
yl-zeng <ylinzeng69@gmail.com>
2017-09-08 22:32:50 -0400

    indexpage responsive

--- app/components/IndexPage.jsx
+++ app/components/IndexPage.jsx
@@ -7,13 +7,15 @@ var IndexPage = React.createClass({
 
   render: function(){
     return (
+      <div>
         <div id="section-main" className="container">
             <Video/>
-            <h1 className="text-center heading">Technology doesn't change the world <br/><br/> people do.</h1>
             <div id="Bigin" className="animated infinite slideInDown">
               <Link to="/home" style={{color:"white"}}><i className="fa fa-angle-double-down fa-5x"></i></Link>
             </div>
         </div>
+        <h1 className="heading">Technology doesn't change the world people do.</h1>
+      </div>
     );
   }
 });
